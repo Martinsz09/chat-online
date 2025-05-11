7b2b99905beec54636c22ba18e408d7dce1210db
mateusbelmiro09@gmail.com <mateusbelmiro09@gmail.com>
2025-05-11 17:10:39 -0300

    alterando server.js e db.js

--- database/db.js
+++ database/db.js
@@ -1,17 +1,13 @@
-// db.js
-const mongoose = require('mongoose');
-require('dotenv').config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-     
-    });
-    console.log('🟢 Conectado ao MongoDB');
-  } catch (err) {
-    console.error('🔴 Erro ao conectar ao MongoDB:', err);
-    process.exit(1); 
-  }
-};
+const mongoose = require('mongoose');
+
+function connectDB() {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("🟢 Conectado ao MongoDB com .env"))
+  .catch((err) => console.error("🔴 Erro ao conectar:", err));
+}
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,28 +1,18 @@
-// server.js
 const express = require('express');
-const connectDB = require('./database/db');
 const app = express();
-const routes = require('./routes/index');
-const path = require('path');
+const router = require('./routes/index');
+const connectDB = require('./database/db');
+require('dotenv').config();
 
-
-connectDB();
-
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
+connectDB();
+app.use('/', router);
 
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use('/', routes);
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+app.listen(3000, () => {
+  console.log('🚀 Servidor rodando na porta 3000');
 });
